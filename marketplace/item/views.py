f9713c95a6be297bddcecb5690256eed42040ae6
Samuel Gyinae <dev.gyinae@gmail.com>
2026-03-24 13:23:13 +0000

    related items

--- marketplace/item/views.py
+++ marketplace/item/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import item  
 
-# Create your views here.
+def detail(request, pk):
+    current_item = get_object_or_404(item, pk=pk)
+    related_items = item.objects.filter( 
+        category=current_item.category, 
+        is_sold=False
+    ).exclude(pk=pk)[0:2]
+    
+    return render(request, 'item/detail.html', {
+        'item': current_item,
+        'related_items': related_items,
+    })
--- marketplace/marketplace/urls.py
+++ marketplace/marketplace/urls.py
@@ -1,13 +1,14 @@
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
 
 from core.views import index, contact, services, products
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', index, name='index'),
+    path('items/', include('item.urls')),
     path('contact/', contact, name='contact'),
     path('services/', services, name='services'),
     path('products/', products, name='products'),
